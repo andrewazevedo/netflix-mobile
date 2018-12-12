0a10b11c94f125e1b4c2cd63c73cc5387ee643dd
Andrew Azevedo <andrew.dias2@gmail.com>
2018-12-12 01:17:35 -0200

    Config eslint and babel module resolver

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/**
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+type Props = {};
+
+class App extends Component<Props> {
+  render() {
+    return (
+      <View>
+        <Text>Netflix app</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
